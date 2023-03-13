8b59aa7119cb0486cf2cff1fc2034afd7337149a
renoob <98941290+renoob21@users.noreply.github.com>
2023-03-13 14:54:09 +0700

    Selesai post seksi Hello, world!

---
+++ 02_Hello, World!/02-Penggunaan Print secara Umum.py
@@ -0,0 +1,25 @@
+# Print dengan variabel
+
+hewan1 = "kucing"
+hewan2 = "naga"
+bunga1 = "mawar"
+
+print(hewan1)
+print(hewan2)
+print(bunga1)
+
+# Print dengan beberapa argumen sekaligus
+
+print(hewan1, hewan2, bunga1)
+
+print(hewan1, "kuda", bunga1, 10)
+
+# Argumen sep= dan end=
+
+# sep
+print(hewan1, "kuda", bunga1, 10, sep="-")
+
+# end
+print(hewan1, end=" ")
+print(hewan2, end="/")
+print(bunga1)
